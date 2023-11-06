3325b5a213185cb878714cff504d493ce779f5b6
Larissa dos Santos Nunes - LNN <larissa.nunes@iteris.com.br>
2023-11-06 13:47:32 -0300

    feat: trabalhando com rotas

---
+++ react-router/src/pages/notfound/index.tsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom'
+
+export function NotFound(){
+  return(
+    <div>
+      <h1>Ops essa página nao existe!!</h1>
+
+      <Link to="/">Acessar página home</Link>
+    </div>
+  )
+}
---
+++ react-router/src/pages/sobre/index.tsx
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom'
+
+export function Sobre(){
+  return(
+    <div>
+      <h1>Página Sobre!</h1>
+      <Link to="/">Ir para home</Link>
+    </div>
+  )
+}
