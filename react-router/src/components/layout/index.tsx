370b2d13f1f0454e189748aed6d84f7fa29fbcc8
Larissa dos Santos Nunes - LNN <larissa.nunes@iteris.com.br>
2024-01-04 09:20:10 -0300

    docs: add comment

--- react-router/src/components/layout/index.tsx
+++ react-router/src/components/layout/index.tsx
@@ -3,9 +3,16 @@ import { Header } from "../header";
 
 export function Layout(){
     return(
+        // Tudo o que está acima do outlet vai ser redenzirado como filho 
+        // Header sempre fixo em cima e conteudo dentro
+        // Footer fixado embaixo (não é filho)
         <>
         <Header/>
         <Outlet/>
+        <br /><br />
+        <footer>
+            <span>Todos os direitos reservados @2024</span>
+        </footer>
         </>
     )
 }
